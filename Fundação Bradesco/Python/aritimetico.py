31a4c7d06e45f16e2f1d06cb899c6ba74c3357d9
Nalu <pordosol699@gmail.com>
2025-08-22 11:38:17 -0300

    aprendnendo os atributos de python

---
+++ Fundação Bradesco/Python/aritimetico.py
@@ -0,0 +1,25 @@
+# Operação	    Símbolo
+# Adição	        +
+# Subtração	        -
+# Multiplicação	    *
+# Divisão Real	    /
+# Divisão Inteiro	//
+# Exponenciação	    **
+# Resto da Divisão	%
+
+# Descrição	      Símbolo
+# Igual a	        ==
+# Diferente de	    !=
+# Maior que	        >
+# Menor que	        <
+# Maior ou igual a	>=
+# Menor ou igual a	<=
+
+
+a = 5
+b = 15
+c = 20
+
+print("a == b AND b > c", a == b and b > c) 
+print("a < b or b > c", a < b or b > c)
+print("not a == b",not a == b)
