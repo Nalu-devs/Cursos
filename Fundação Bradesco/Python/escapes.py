1f1653db032e3998f4a5395ea3975ac162d35428
Nalu <pordosol699@gmail.com>
2025-08-22 11:31:05 -0300

    escapes e como utilizar

---
+++ Fundação Bradesco/Python/escapes.py
@@ -0,0 +1,11 @@
+# \n	Insere uma quebra de linha.
+# \t	Insere tabulação horizontal.
+# \v	Insere tabulação vertical.
+# \r	Equivalente ao efeito da tecla Enter.
+# \’	Aspas simples.
+# \”	Aspas duplas.
+# \\	Insere uma barra invertida (backslash).
+# \a	Chamado de asc bell ou beep do sistema. Se houver suporte, aciona um bipe.
+# \b	Aciona o backspace, ou seja, apaga o caractere anterior.
+# \f	Insere uma quebra de página.
+# \u	Insere um caractere unicode. Deve acompanhar um código com 4 números.
