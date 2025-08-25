99ce3f864de778bd51ed144d333eac6ce19b2826
Aluno.Etec <aula@etec.com>
2025-08-25 20:44:09 -0300

    .

---
+++ Fundação Bradesco/Linguagem de programação Python/Introdução à Programação Orientada a Objetos (POO)/Os_quatro_pilares_da_programação_orientada_a_objetos.py
@@ -0,0 +1,11 @@
+# programação orientada a objeto, explicando seus quatro mais importantes componentes: abstração, encapsulamento, herança e polimorfismo. 
+
+# ABSTRAÇÃO: processo de aproximar o mundo real do mundo da programação, sendo o seu objetivo, simplificar um problema difícil
+# a abstração se concentra apenas nas informações que são importantes para o seu propósito. Dessa forma, ela mantém suas classes 
+# o mais simples possível, concentrando-se apenas no que é importante para uma determinada finalidade. EXEMPLO: 
+
+# Vamos supor que você está desenvolvendo um código com a classe “ser humano”. Essa classe é composta por inúmeras variantes (atributos), 
+# como por exemplo: altura, peso, cor da pele, cor do olho, CPF, nome, endereço etc.
+# O objetivo do seu código é tratar o "ser humano" como cliente. Para cada cliente é preciso obter as informações de CPF, Nome e Endereço.
+# As outras informações que pode obter na classe "ser humano", como cor da pele, cor do olho, altura, peso etc. não são importantes para 
+# esta situação. Portanto, podemos abstrair estas informações e considerar somente o que importa.
