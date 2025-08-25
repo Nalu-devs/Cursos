8ef7cc2a45f57b4febbf6c87a22f3e40db44ca64
Aluno.Etec <aula@etec.com>
2025-08-25 20:14:17 -0300

    algumas normas da POO

--- Fundação Bradesco/Linguagem de programação Python/Introdução à Programação Orientada a Objetos (POO)/Programação_estruturada_versus_orientada_a_objetos.py
+++ Fundação Bradesco/Linguagem de programação Python/Introdução à Programação Orientada a Objetos (POO)/Programação_estruturada_versus_orientada_a_objetos.py
@@ -2,4 +2,17 @@
 # Ou seja, a sua lógica é direcionada à linguagem de máquina, já que ela realiza o que foi
 # orientado pelo programador por meio de uma linguagem estruturada.
 
-Programação orientada a objetos
+# Programação orientada a objetos funciona de um modo mais similar ao raciocínio humano.
+# Os seus códigos são organizados de acordo com objetos, e não processos, como no caso da programação estruturada
+
+# OBJETO: São elementos do mundo real para o mundo da programação. O objeto é criado a partir de uma classe.
+# É algo que se visualiza, se utiliza e assume um papel no domínio do problema. Um exemplo disso, é um Fusca 1964.
+
+# ATRIBUTOS: São as características do objeto. Considerando o exemplo do Fusca 1964, os atributos deles são:
+# ano de fabricação é 1964 e que ele tem um formato arredondado e o motor fica na parte traseira.
+
+# METODOS: São os comportamentos do objeto. Ainda de acordo com o exemplo, o Fusca pode se locomover, transportando pessoas, cargas etc.
+
+# CLASSES: São os agrupamentos que descrevem todos os objetos de um único tipo. Por exemplo, o fusca pertence à classe dos carros. 
+# Não importa a sua marca ou ano. O que importa é que são objetos que se encaixam nesta classe, que serve como molde para a criação 
+# dos objetos. É na classe que são moldados os objetos que serão utilizados do programa a ser desenvolvido.
