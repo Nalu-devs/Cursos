494425ca30d5d3f07556dedfa07edd807bfd2227
Nalu <pordosol699@gmail.com>
2025-08-25 09:07:51 -0300

    introdução

---
+++ Fundação Bradesco/Linguagem de programação Python/Introdução à Programação Orientada a Objetos (POO)/Programação_estruturada_versus_orientada_a_objetos.py
@@ -0,0 +1,5 @@
+# programacao estruturada segue uma lógica e sequência de pensamentos de uma máquina.
+# Ou seja, a sua lógica é direcionada à linguagem de máquina, já que ela realiza o que foi
+# orientado pelo programador por meio de uma linguagem estruturada.
+
+Programação orientada a objetos
