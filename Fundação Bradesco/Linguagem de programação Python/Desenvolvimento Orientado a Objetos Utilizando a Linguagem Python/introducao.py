995f571f8f8f2f3c126928cab204f1ceed0fd821
Nalu <pordosol699@gmail.com>
2025-08-26 11:07:43 -0300

    INTRODUZINDO ALGUNS CONCEITOS EM PYTHON

---
+++ Fundação Bradesco/Linguagem de programação Python/Desenvolvimento Orientado a Objetos Utilizando a Linguagem Python/introducao.py
@@ -0,0 +1,44 @@
+# CLASSES:
+# Definição do Objeto
+# Conjunto de atributos e funções utilizado como modelo para criação dos objetos.
+
+# OBJETOS:
+# Instância de uma classe
+# Variável que possui todas as características comuns à classe, porém, com valores
+# diferentes em seus atributos.
+
+# ABSTRACAO:
+# Acesso às utilidades da classe
+# Ação de utilizar mensagens para acessar os recursos de uma classe.
+
+# ATRIBUTOS:
+# Características da classe
+# Os atributos são características do elemento que a classe representa.
+
+# FUNÇÕES:
+# Ações do objeto com retorno
+# Função nativa do Python que retorna um valor declarado dentro da classe.
+
+# METODOS:
+# Ações do Objeto
+# Função nativa do Python que não possui retorno declarado dentro de uma classe.
+
+# EXCEÇÃO:
+# Tratamento de erros
+# Controla o fluxo de execução durante um erro.
+
+# MENSAGEM:
+# Chamada a um atributo, método ou função.
+
+# HERANÇA:
+# Super classe e Sub Classe
+# Uma classe pode herdar atributos, métodos e/ou funções de outra.
+
+# ENCAPSULAMENTO:
+# Níveis de acesso
+# O encapsulamento permite que os atributos sejam vistos somente nas classes onde foram
+# declarados, definindo o nível de acesso de atributos, métodos ou funções.
+
+# POLIMORFISMO:
+# Sobrescrita
+# Escolher entre os atributos, métodos e/ou funções que sobrescreveram ou que foram sobrescritos.
