705442477e6c67bc870dcbdecad0b01708d6b79f
Nalu <pordosol699@gmail.com>
2025-08-27 10:06:00 -0300

    aprendnendo como manipular dados com string

---
+++ Fundação Bradesco/Linguagem de programação Python/Desenvolvimento Orientado a Objetos Utilizando a Linguagem Python/string.py
@@ -0,0 +1,14 @@
+# O tipo String é conhecido como uma cadeia de caracteres. Podemos imaginar uma 
+# string como uma sequência de blocos onde cada letra, número ou espaço em branco
+# ocupa uma posição.
+
+# Os textos podem ser escritos em aspas simples, duas e tres aspas para multiplas linhas
+
+# FUNÇÕES PARA STRING:
+# Função len(): O tamanho de uma string pode ser obtido utilizando a função len(). 
+# Essa função retorna o número de caracteres armazenados em uma string. Ela retorna 
+# um único valor, indicando a quantidade de caracteres contidos na variável.
+
+class Main:
+    nome = "Ana"
+    print(len(nome))
