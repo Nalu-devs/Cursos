627efec6cc4ae7e183b27eb1ed2941dedb9a7f81
Nalu <pordosol699@gmail.com>
2025-08-27 10:59:32 -0300

    ENTENDENDO O CONCEITO DE WIDGET

---
+++ Fundação Bradesco/Linguagem de programação Python/Criando um Projeto com Interface Gráfica Utilizando a Linguagem Python/introducao.py
@@ -0,0 +1,22 @@
+# Quando estamos acessando uma rede social, um jogo no celular, um aplicativo da área 
+# de trabalho do computador, ou até mesmo um site, estamos interagindo com o que chamamos 
+# de aplicativo ou widget, a depender da situação.
+# Talvez o termo widget tenha se originado da junção das palavras em inglês window 
+# (janela) e gadget (dispositivo), o que jamais poderemos saber, pois esta é uma 
+# especulação na comunidade tecnológica.
+
+# FLUTTER:
+# É um framework desenvolvido pelo Google para criação de aplicaçõesmóveis multiplataforma 
+# (Android e iOS), utilizando uma única base decódigo, e muito aclamado por sua facilidade 
+# em desenvolver aplicaçõescom uma interface intuitiva eagradável. Para a criação dessas 
+# interfaces,o Flutter utiliza um conceito de widgets
+
+# O QUE SÃO APLICATIVOS:
+# Quando falamos sobre aplicativos, podemos remeter a um marco de suas histórias: em 1983, 
+# o jovem Steve Jobs idealizou o que conhecemos hoje como Apple Store. Ele apresentou a ideia
+# em uma conferência em Aspen naquele ano.
+# "A estratégia da Apple é realmente simples. O que nós queremos écolocar um computador 
+# incrivelmente ótimo em um livro que vocêpossa levar com você e aprender a usar em 20 minutos. 
+# Isso é o quequeremos fazer, e ainda nesta década. E nós realmente queremos fazerisso com um link 
+# de rádio, para que você não tenha que conectá-lo anada, e para se conectar com vários bancos de
+# dados maiores e outroscomputadores”. (JOBS, 1983 apud CHAN, 2012)
