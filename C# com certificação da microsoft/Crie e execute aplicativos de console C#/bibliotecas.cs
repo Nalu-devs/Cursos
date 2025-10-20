9754c9ee58bb2fba9e9c197f6500bbd3f4c37acc
Nalu <pordosol699@gmail.com>
2025-10-20 08:36:24 -0300

    APRENDENDO A USAR BIBLIOTECAS

---
+++ C# com certificação da microsoft/Crie e execute aplicativos de console C#/bibliotecas.cs
@@ -0,0 +1,71 @@
+// Quando você precisa implementar uma tarefa de programação, a Biblioteca de Classes do .NET é um bom 
+// lugar para procurar, pois é uma coleção organizada de recursos de programação.
+// A Biblioteca de Classes do .NET é uma coleção de milhares de classes que contém dezenas de milhares 
+// de métodos. Por exemplo, ela inclui a classe Console para desenvolvedores que trabalham em 
+// aplicativos de console. A classe Console inclui métodos para operações de entrada e saída, 
+// como Write(), WriteLine(), Read(), ReadLine() e muitos outros. Por exemplo:
+Console.WriteLine("Hello, World!")
+
+// Pense em uma classe como um contêiner para métodos ou como as seções da biblioteca pública. Os 
+// desenvolvedores normalmente mantêm os métodos relacionados juntos em uma única classe. Como você 
+// viu no exemplo anterior, todos os métodos que podem enviar ou receber informações de uma janela de 
+// console são coletados na classe System.Console da biblioteca de classes do .NET.
+// Existem outras classes com métodos que dão suporte de uma forma mais geral. Em outras palavras, sua 
+// utilidade abrange uma ampla variedade de plataformas de dispositivos, estruturas de aplicativos e 
+// áreas de tecnologia. Por exemplo, se você quiser ler ou gravar informações de arquivos ou executar 
+// operações de trigonometria ou cálculo, há classes de uso geral que podem ser usadas em seu código.
+// Não importa se você está criando aplicativos para a Web, a área de trabalho, um dispositivo 
+// móvel ou a nuvem, as classes e os métodos de uso geral são uma opção útil.
+
+
+
+
+// ATÉ MESMO OS TIPOS DE DADOS FAZEM PARTE DA BIBLIOTECA DE CLASSES DO .NET
+// Na verdade, os tipos de dados em C# (como string e int) são disponibilizados por meio de classes 
+// na biblioteca de classes do .NET. A linguagem C# mascara a conexão entre os tipos de dados e as 
+// classes do .NET para simplificar seu trabalho. No entanto, nos bastidores, os tipos de dados são 
+// implementados como todas as outras classes na Biblioteca de Classes do .NET. Essa conexão fornece 
+// suas variáveis cotidianas com métodos internos que podem ser muito úteis. A classe string tem muitos 
+// desses métodos úteis. Por exemplo, a classe string tem métodos para converter texto em letras 
+// maiúsculas e minúsculas (ToUpper e ToLower).
+
+
+
+
+// COMO ENCONTRAR O QUE VOCÊ PRECISA NA BIBLIOTECA DE CLASSES DO .NET
+// Em primeiro lugar, lembre-se de que encontrar todas as classes e métodos na biblioteca de classes do 
+// .NET é como encontrar todos os livros em uma grande biblioteca pública. Você não precisa de todos 
+// os livros da biblioteca e não usará todas as classes e os métodos da biblioteca de classes do .NET. 
+// Dependendo dos tipos de projetos nos quais você trabalha, você se familiarizará mais com algumas 
+// partes da biblioteca de classes do .NET e menos com outras
+// Em segundo lugar, a necessidade o levará ao que você precisa. A maioria das pessoas vai à biblioteca 
+// quando precisa encontrar um livro, não para ver quantos livros diferentes elas podem encontrar. 
+// Você não precisa pesquisar classes e métodos sem um motivo
+// Em terceiro lugar, a Microsoft fornece uma referência de linguagem online e um guia de programação 
+// para C# que podem ser utilizados em suas pesquisas
+
+
+
+
+// EXERCICIO - CHAMAR OS METODOS DE UMA CLASSE .NET
+// Com base em sua experiência anterior com o método Console.WriteLine(), você já deve conhecer os 
+// conceitos básicos:
+
+// 1 - Comece digitando o nome da classe. Neste caso, o nome da classe é Console.
+// 2 - Adicione o operador de acesso de membro, o símbolo ..
+// 3 - Adicione o nome do método. Neste caso, o nome do método é WriteLine.
+// 4 - Adicione o operador de chamada do método, que é um conjunto de parênteses ().
+// 5 - Finalmente, especifique os argumentos que serão transmitidos ao método, se houver algum, 
+// entre os parênteses do operador de invocação de método. Aqui, você especifica o texto que deseja 
+// que o método Console.WriteLine() escreva no console (por exemplo, "Hello World!").
+
+// dependendo de como os desenvolvedores criaram e implementaram o método especificado, você também 
+// precisará:
+
+// 6 - Passar valores adicionais como parâmetros de entrada.
+// 7 - Aceitar um valor retornado.
+
+
+
+
+// PREPARAR O AMBIENTE DE CODIFICAÇÃO
