8bf39fc2c7d2d8aa767baf406e08db3a03f37ac9
Nalu <pordosol699@gmail.com>
2025-10-20 09:05:02 -0300

    vendo como funciona um metodo

---
+++ C# com certificação da microsoft/Crie e execute aplicativos de console C#/metodos.cs
@@ -0,0 +1,6 @@
+// para criar um exemplo de código que implemente métodos das classes System.Random e System.Console
+Random aleatorio = new Random();
+int rolar = aleatorio.Next(1, 7);
+Console.WriteLine(rolar);
+// Este código simula um lançamento de dados usando o método Random.Next() para gerar um número e o 
+// método Console.WriteLine() para exibir o valor.
